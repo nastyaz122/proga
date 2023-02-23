8bdb3ab9a94192d07e4568f310622bfc28a6c3f3
nastyaz122 <115534094+nastyaz122@users.noreply.github.com>
2023-02-23 14:56:03 +0300

    Update prog.cpp

--- prog.cpp
+++ prog.cpp
@@ -1,41 +1,55 @@
 #include <iostream>
+#include <vector>
+#include <cmath>
 using namespace std;
 
-int main()
+class shape {
+public:
+	virtual void perimeter() {};
+};
+
+class circle :public shape {
+private:
+	int r;
+	double PI = 3.14;
+public:
+	
+	void perimeter() {
+		double p = 2 * PI * r;
+	}
+	circle(int r)
+	{
+		this->r = r;
+	}
+};
+
+class square :public shape {
+private:
+	int a;
+public:
+	void perimeter(){
+		double p = 4 * a;
+	}
+	square(int a)
+	{
+		this->a = a;
+	}
+};
+
+int main(int argc, char* argv[])
 {
-	int a[3][3];
-	for (int i = 0; i < 3; i++)
-	{
-		for (int j = 0; j < 3; j++)
-		{
-			a[i][j] = rand() % 10;
-		}
+	vector<shape*>v;
+	v.push_back(new circle(5));
+	v.push_back(new square(5));
+	v.push_back(new circle(5));
+	v[0]->perimeter();
+	for (int i = 0; i < 2 ; i++) {
+		cout<< v[i] << " ";
 	}
-	for (int i = 0; i < 3; i++)
-	{
-		for (int j = 0; j < 3; j++)
-		{
-			cout << a[i][j] << " ";
-		}
+
+	for (int i = 0; i < 2; i++) {
+		delete v[i]; v[i] = 0;
 	}
-	cout << endl;
-	int sum = 0;
-	double arif = 0;
-	for (int i = 0; i < 3; i++)
-	{
-		for (int j = 0; j < 3; j++)
-		{
-			if (!((i + j) % 2 == 0))
-			{
-				if (a[i][j] % 2 == 0)
-				{
-					 arif = arif + a[i][j];
-					 sum = sum + 1;
-				}
-			}
-		}
-	}
-	double arr = arif / sum;
-	cout << arr << endl;
 	return 0;
 }
+
